0cae91703ed6aa40c9d871494deae608ba0afe11
SerjoschDuering <tralala798@gmail.com>
2022-10-21 11:42:36 +0200

    Add websocket js script client

---
+++ websocket_client.js
@@ -0,0 +1,22 @@
+
+// https://www.youtube.com/watch?v=NVyeeZsYhF8
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("123");
+
+    const websocketClient = new WebSocket("ws://localhost:12345/");
+    //const messageContainer = document.querySelector("#messageContainer");
+    //constMessageButton =  doctument-querySelector("[name=send_message_button]");
+
+    websocketClient.onopen = function(){
+        console.log("Client Connected");
+
+        /*
+        sendMessageButton.onclick = function(){
+            console.log("click");
+        };
+        */
+       
+        websocketClient.send("hello");
+    }
+
+}, false)
